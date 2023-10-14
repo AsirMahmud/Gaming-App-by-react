866d66f8e07a4f8676bf06f75fa98c80f49d0c66
AsirMahmud <email@asirmahmuhhdd@gamil.com>
2023-10-14 10:22:02 +0600

    built a responsive layout

--- gaming-app/src/App.tsx
+++ gaming-app/src/App.tsx
@@ -1,10 +1,28 @@
-import { Button } from "@chakra-ui/react";
+import { Button, Grid, GridItem, Show } from "@chakra-ui/react";
 import { color } from "framer-motion";
 
 const App = () => {
   return (
     <div>
-      <Button flex={}>ASIR</Button>
+      <Grid
+        templateAreas={{
+          base: `"nav " " main"`,
+          lg: `"nav nav" "aside main"`,
+        }}
+      >
+        <GridItem area="nav" bg="tomato">
+          Nav
+        </GridItem>
+        <Show above="lg">
+          {" "}
+          <GridItem area="aside" bg="teal">
+            Aside
+          </GridItem>
+        </Show>
+        <GridItem area="main" bg="black">
+          Main
+        </GridItem>
+      </Grid>
     </div>
   );
 };
